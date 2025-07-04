b024207930a71ced45aaab571db97378f48ebaae
loveCiForever <quanghuy71847@gmail.com>
2025-07-05 00:33:42 +0700

    Update main for module testing

--- financial_news_sentiment_analysis_with_phobert/src/crawling/main.py
+++ financial_news_sentiment_analysis_with_phobert/src/crawling/main.py
@@ -2,15 +2,16 @@
 
 import time
 import pandas as pd
-from src.crawling.runner import run
+from src.crawling.runner import crawling_run
 from src.utils import format_text
 
 if __name__ == "__main__":
     start_time = time.time()
 
-    merged_df = run()
-    output = "data/raw/dataset.csv"
+    merged_df = crawling_run(bao_dau_tu_max_page=1, cafef_max_page=1)
+    output = "data/raw/test.csv"
     merged_df.to_csv(output, index=False, encoding='utf-8-sig')
+    print(merged_df.sample(1).iloc[0])
 
     end_time = time.time()
     elapsed_time = end_time - start_time
@@ -19,5 +20,6 @@ if __name__ == "__main__":
         format_text(
             text=f"[TIME] Finish crawling news in runtime: {elapsed_time:.2f} seconds",
             fg="cyan",
+            style="blink"
         )
     )
